426942918aa06b2de41039c7c9ca147cb34090a2
Pollyco <avlispollyana@outlook.com>
2025-08-01 17:37:43 -0400

    Add files via upload

---
+++ sistema_bancario.py
@@ -0,0 +1,89 @@
+mensagem = "Extrato"
+menu = """
+
+[1] Depósito
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+=> """
+
+saldo = 1500
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+
+        else:
+            print("Operação inválida! O valor informado está incorreto.")
+
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação inválida! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação inválida! Número máximo de saques excedido.")
+
+        elif excedeu_saques:
+            print("Operação inválida! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n" 
+            numero_saques += 1
+
+        else:
+            print("Operação inválida! O valor informado está incorreto.")  
+
+    elif opcao == "3":
+        print("=========="+ mensagem +"==========")  
+        print("Não foram realizadas movimentações" if not extrato else extrato)       
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        
+
+    elif opcao == "0":
+        break
+
+    else:
+        ("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
